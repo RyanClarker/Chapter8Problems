d5eab1e5dfa1cf905cf80ba3c23cfc9ff45c929f
RyanClarker <clarke.ryan24@gmail.com>
2014-05-28 10:09:18 -0400

    new

---
+++ Chapter8Problems/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter8Problems
+{
+    class Pizza
+    {
+        private string topping;
+        public string Topping
+        {
+            get
+            {
+                return topping;
+            }
+            set
+            {
+                topping = value;
+            }
+        }
+
+        private int diameter;
+        public int Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+            set
+            {
+                diameter = value;
+            }
+        }
+
+        private double cost;
+        public double Cost
+        {
+            get
+            {
+                return cost;
+            }
+            set
+            {
+                cost = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza = new Pizza();
+
+            pizza.Topping = "Cheese";
+            pizza.Diameter = 12;
+            pizza.Cost = 3.14;
+
+        }
+    }
+}
+
+
+            
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class HousePlant
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private double price;
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+        public bool Fed { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HousePlant plant = new HousePlant();
+            HousePlant palm = new HousePlant();
+            HousePlant tree = new HousePlant();
+
+            palm.Name = "Palm Tree";
+            palm.Price = 39.95;
+            palm.Fed = false;
+
+            plant.Name = "Rose";
+            plant.Price = 11.95;
+            plant.Fed = true; 
+
+            tree.Name = "Woody";
+            tree.Price = 999.99;
+            tree.Fed = true;
+
+            Console.WriteLine(palm.Name);
+            Console.WriteLine(palm.Price);
+            Console.WriteLine(palm.Fed);
+            Console.WriteLine();
+            Console.WriteLine(plant.Name);
+            Console.WriteLine(plant.Price);
+            Console.WriteLine(plant.Fed);
+            Console.WriteLine();
+            Console.WriteLine(tree.Name);
+            Console.WriteLine(tree.Price);
+            Console.WriteLine(tree.Fed);
+
+        }
+    }
+}
