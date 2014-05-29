22ff9707a011e278f5a435b74598eb21c1e523f8
RyanClarker <clarke.ryan24@gmail.com>
2014-05-29 10:09:27 -0400

    new

---
+++ DemoSquares.cs/Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoSquares.cs
+{
+    class Square
+    {
+        private double area;
+        private double length;
+
+        public double Constructor(double Length)
+        {
+            this.length = Length;
+
+            private double Compute
+            {
+                get
+                {
+                  return area;
+                }
+                set
+                {
+                    area = value;
+                    Calc();
+                }
+            }
+        private void Calc()
+        {
+            area = length * length;
+        }
+    }
+}
+
+
+            
---
+++ TestCircles/TestCircles.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCircles
+{
+    class TestCircles
+    {
+        private double radius;
+        private double diameter;
+        private double area;
+
+        public TestCircles()
+        {
+            this.radius = 1;
+            Calc();
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+                Calc();
+            }
+        }
+
+        public double Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                return area;
+            }
+        }
+
+        private void Calc()
+        {
+            diameter = radius * 2;
+            area = Math.PI * radius * radius;
+        }
+     }
+    class Program
+    {
+        public static void Main()
+        {
+            TestCircles testcircle = new TestCircles();
+
+            TestCircles test1 = new TestCircles();
+            TestCircles test2 = new TestCircles();
+            TestCircles test3 = new TestCircles();
+
+            test1.Radius = 5;
+            test2.Radius = 100;
+
+            Console.WriteLine("{0} -> Radius={1,5}  Diameter={2,5}  Area={3,5}", "test1", test1.Radius, test1.Diameter, test1.Area);
+            Console.WriteLine("{0} -> Radius={1,5}  Diameter={2,5}  Area={3,5}", "test2", test2.Radius, test2.Diameter, test2.Area);
+            Console.WriteLine("{0} -> Radius={1,5}  Diameter={2,5}  Area={3,5}", "test3", test3.Radius, test3.Diameter, test3.Area);
+        }
+    }
+}
+
+        
+    
